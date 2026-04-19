48872527edadd080897b427a85a2b6ec91f1c9cd
Ganga Singh <gangasingh1734@gmail.com>
2026-04-19 14:09:24 +0530

    added the loaders

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,10 @@
+import { Loader2 } from 'lucide-react'
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className="loading flex justify-center italic h-screen items-center">
+        <Loader2 className="animate-spin" size={40} />
+    </div>
+  )
+}
